e158d8fcfb4f298a4dbd6b9a4e8fba4c3ef35c2e
David Rae <daraeman@gmail.com>
2018-03-27 10:57:56 -0700

    fixes #1

--- ebay_include_shipping.user.js
+++ ebay_include_shipping.user.js
@@ -34,10 +34,14 @@
 
 		$( "#ListViewInner .sresult" ).each( ( i, node ) => {
 			let el = $( node );
-			let item_text = el.find( ".lvprices .lvprice .bold" ).text().trim();
+//			console.log( "el", el )
+			let item_text = getItemPriceEl( el ).text().trim();
+//			console.log( "item_text", item_text )
 			let currency = item_text[0];
+//			console.log( "currency", currency )
 			let item_price = new Big( item_text.substr( 1 ) );
-			let shipping_text = el.find( ".lvshipping .ship .fee" ).text().trim().substr( 2 ).replace( "shipping", "" ).trim();	
+//			console.log( "item_price", item_price )
+			let shipping_text = getShippingPriceEl( el ).text().trim().substr( 2 ).replace( "shipping", "" ).trim();	
 			if ( shipping_text ) {
 				let shipping_price = new Big( shipping_text );
 				addSearchItemShippingPrice( el, item_price.plus( shipping_price ), currency );
@@ -45,16 +49,32 @@
 		});
 	}
 
+	function getItemPriceEl( parent, post_add ) {
+		let selector = ( post_add ) ? ".bold:not( .ebay_include_shipping )" : ".bold";
+		let this_parent = getShippingPriceParentEl( parent );
+		return this_parent.find( selector );
+	}
+
+	function getShippingPriceEl( parent ) {
+		return parent.find( ".lvshipping .ship .fee" );
+	}
+
+	function getShippingPriceParentEl( parent ) {
+		return parent.find( ".lvprices .lvprice" );
+	}
+
 	function addSearchItemShippingPrice( el, price, currency ) {
-		let price_parent_el = el.find( ".lvprices .lvprice" );
+		let price_parent_el = getShippingPriceParentEl( el );
 		price_parent_el.prepend( '<span class="bold ebay_include_shipping">'+ currency + price.toFixed( 2 ).toString() +' with shipping</span><br>' );
-		let item_price_el = price_parent_el.find( ".bold:not( .ebay_include_shipping )" );
-		item_price_el.text(  "(" + item_price_el.text().trim() + ")" );
+		let item_price_el = getItemPriceEl( el, true );
+		let shipping_price_el = getShippingPriceEl( el );
+		item_price_el.text( "(" + item_price_el.text().trim() + shipping_price_el.text().trim().replace( "+", " + " ) +")" );
 		item_price_el.removeClass( "bold" ).css( {
 			"display": "inline-block",
 			"margin-top": "4px",
 			"font-size": "13px",
 		});
+		shipping_price_el.hide();
 	}
 
 	function init() {
